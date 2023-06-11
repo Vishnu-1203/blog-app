7eb5cd77910cd9f6bd8ea0e8b61eed118ea684fb
Vishnu-1203 <vishnuda333@gmail.com>
2023-06-11 19:12:35 +0530

    3rd submit
    textbar almost done now add the posting feature

--- src/components/blogItems/textBar.tsx
+++ src/components/blogItems/textBar.tsx
@@ -1,37 +1,64 @@
 import React, { useState } from "react";
 import "./blogStyles.css";
 
-const TextBar = () => {
+const TextBar: React.FC = () => {
   const [inputText, setInputText] = useState("");
-  const [blogBlocks, setBlogBlocks] = useState<string[]>([]);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [postDetails, setPostDetails] = useState<any[]>([]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(event.target.value);
   };
 
-  const handleAddBlock = () => {
-    if (inputText.trim() !== "") {
-      setBlogBlocks((prevBlocks) => [...prevBlocks, inputText]);
-      setInputText("");
-    }
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(event.target.value);
+  };
+
+  const handlePostSubmit = (event: React.FormEvent) => {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+    setPostDetails([...postDetails, { name, date, inputText }]);
+    setInputText("");
+    setName("");
+    setDate("");
+    console.log(postDetails);
   };
 
   return (
-    <div className="text-bar">
-      <input
-        type="text"
-        value={inputText}
-        onChange={handleInputChange}
-        placeholder="Enter your text"
-      />
-      <button onClick={handleAddBlock}>Add Block</button>
-      <div className="blog-area">
-        {blogBlocks.map((block, index) => (
-          <div key={index} className="blog-block">
-            {block}
+    <div className="TextBar">
+      <form onSubmit={handlePostSubmit}>
+        <div className="TextBar-content">
+          <div className="InputContainer">
+            <input
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+              placeholder="Enter your name"
+              className="NameInput"
+            />
+            <input
+              type="text"
+              value={date}
+              onChange={handleDateChange}
+              placeholder="Enter the date"
+              className="DateInput"
+            />
+            <button type="submit" className="submitButton">
+              Post
+            </button>
           </div>
-        ))}
-      </div>
+          <textarea
+            value={inputText}
+            onChange={handleInputChange}
+            placeholder="Enter your text"
+            className="TextArea"
+          />
+        </div>
+      </form>
     </div>
   );
 };
